0443d9574b950d4f315fdc2e10fbe23ba28955dc
xandervr <xandervanraemdonck@gmail.com>
2020-03-03 14:50:24 +0100

    added auth

--- server.js
+++ server.js
@@ -13,41 +13,58 @@ const finalHandler = require('finalhandler');
  * @return {object}
  */
 function server(config = {}) {
-    if (!config.monitor || !config.monitor.enabled) {
-        throw new Error('WebSocket server is not enabled!');
-    }
+  if (!config.monitor || !config.monitor.enabled) {
+    throw new Error('WebSocket server is not enabled!');
+  }
 
-    const { host = '0.0.0.0', port = 7210, socketOpts = {} } = config.monitor;
-    return {
-        /**
-         *
-         * @param {function} cb
-         */
-        listen(cb) {
-            const serve = serveStatic(path.resolve(`${__dirname}/dist`), {
-                index: 'index.html',
-                dotfiles: 'deny'
-            });
-            const server = http.createServer((req, res) => {
-                serve(req, res, finalHandler(req, res));
-            });
-            const io = Socket(server, socketOpts);
+  const { host = '0.0.0.0', port = 7210, socketOpts = {} } = config.monitor;
+  return {
+    /**
+     *
+     * @param {function} cb
+     */
+    listen(cb) {
+      const serve = serveStatic(path.resolve(`${__dirname}/dist`), {
+        index: 'index.html',
+        dotfiles: 'deny'
+      });
+      const server = http.createServer((req, res) => {
+        serve(req, res, finalHandler(req, res));
+      });
+      const io = Socket(server, socketOpts);
 
-            const { redis: options = {} } = config;
-            const client = redis.createClient(options);
-            client.on('ready', () => {
+      const { redis: options = {} } = config;
+      const client = redis.createClient(options);
+      client.on('connect', () => {
+        if (options.password) {
+          client.auth(options.password, err => {
+            if (!err) {
+              client.on('ready', () => {
                 client.subscribe('stats');
-            });
-            client.on('message', (channel, message) => {
+              });
+              client.on('message', (channel, message) => {
                 const json = JSON.parse(message);
                 io.emit('stats', json);
-            });
-            server.listen(port, host, () => {
-                console.log(`Listening for new connections  on ${host}:${port}...`);
-                cb && cb();
-            });
-        },
-    };
+              });
+            } else throw err;
+          });
+        } else {
+          client.on('ready', () => {
+            client.subscribe('stats');
+          });
+          client.on('message', (channel, message) => {
+            const json = JSON.parse(message);
+            io.emit('stats', json);
+          });
+        }
+      });
+
+      server.listen(port, host, () => {
+        console.log(`Listening for new connections  on ${host}:${port}...`);
+        cb && cb();
+      });
+    }
+  };
 }
 
 module.exports = server;
